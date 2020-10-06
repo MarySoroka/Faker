ae4507a3307c03163237a44efa37080c67694bc0
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-06 15:18:40 +0300

    Create project

---
+++ FakerLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FakerLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ FakerPlugins/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FakerPlugins
+{
+    public class Class1
+    {
+    }
+}
